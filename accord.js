ea2674f0c52e38a0b4e798b821dbf3df1aa6f4ed
Mysteryeditor <sri027.cvb@gmail.com>
2023-03-22 16:38:22 +0530

    accordion commit

---
+++ accord.js
@@ -0,0 +1,8 @@
+$("#phoneadd").click(function(){
+   
+    $("#phone").prepend("<img src='/images/phone3.jpeg'>");
+})
+$("#lapadd").click(function(){
+  
+   $("#lap").append("<img src='/images/laptop1.jpg'>");
+})
