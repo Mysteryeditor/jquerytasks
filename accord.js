f4d350125030f2d1c244e7b7492e6318a1bab028
Mysteryeditor <sri027.cvb@gmail.com>
2023-03-24 16:35:43 +0530

    task1commit

--- accord.js
+++ accord.js
@@ -1,10 +1,11 @@
 $("#phoneadd").click(function(){
    
-    $("#phone").prepend("<img src='/images/phone3.jpeg'>");
+    $("#phone").prepend("<img src='/task1/images/phone3.jpeg'>");
 })
 $("#lapadd").click(function(){
   
-   $("#lap").append("<img src='/images/laptop1.jpg'>");
+   $("#lap").append("<img src='/task1/images/laptop1.jpg'>");
 })
+
 
   
